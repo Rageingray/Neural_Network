75dd3ca08b5f6552d06d35b34e1fc915673c6954
Arsh Bansal <111636760+Rageingray@users.noreply.github.com>
2023-08-09 14:35:41 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,26 @@
+import csv
+import os
+
+desktop_path = os.path.expanduser("~/Desktop")
+csv_file_path = os.path.join(desktop_path, r"C:\Users\arshb\Desktop\diabetes.csv")
+
+data = []
+with open(csv_file_path, "r") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        data.append(row)
+
+column_widths = [max(len(str(item)) for item in col) for col in zip(*data)]
+
+for i, header in enumerate(data[0]):
+    print(header.ljust(column_widths[i]), end=" | ")
+print()
+
+for width in column_widths:
+    print("-" * width, end=" | ")
+print()
+
+for row in data[1:]:
+    for i, item in enumerate(row):
+        print(str(item).ljust(column_widths[i]), end=" | ")
+    print()
